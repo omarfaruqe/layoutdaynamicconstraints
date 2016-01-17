e32d6e97006e8998fd01928a013fd8d28533bdc2
Omar Faruqe <faruqe.cse@gmail.com>
2016-01-16 22:35:00 -0500

    Edited Readme

--- LayoutDynamicConstraints/ViewController.m
+++ LayoutDynamicConstraints/ViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "ViewController.h"
+#import "MapKit/MapKit.h"
 
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 
 @end
 
@@ -17,6 +21,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    //setting webview content
+    NSString *webURL = @"https:www.twitter.com";
+    NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:[NSURL URLWithString:webURL]];
+    [self.webView loadRequest:request];
+    
+    //setting mapview content
+    double latitude = 45.4937921;
+    double longitude = -73.5861662;
+    MKPointAnnotation *pointAnnotation = [[MKPointAnnotation alloc] init];
+    pointAnnotation.coordinate = CLLocationCoordinate2DMake(latitude, longitude);
+    pointAnnotation.title = @"Concordia University, Montreal";
+    
+    [self.mapView addAnnotation:pointAnnotation];
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance(pointAnnotation.coordinate, 2000, 2000);
+    MKCoordinateRegion adjustedRegion = [self.mapView regionThatFits:region];
+    [self.mapView setRegion:adjustedRegion animated:YES];
+    
 }
 
 - (void)didReceiveMemoryWarning {
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# layoutdaynamicconstraints
+# Layout with Daynamic Constraints
+
+iOS(Objective-C) 
+Layout with Daynamic Constraints Assignment (Assignment for Week 1) for 
+Coursera "Best Practices for iOS User Interface Design" course under 
+"iOS Development for Creative Entrepreneurs Specialization"
